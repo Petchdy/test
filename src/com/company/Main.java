04a14e8aab6acef7aa3f55220e75342e7b197b49
Petchdy <skulwisit@gmail.com>
2020-04-16 14:22:14 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //C:\Users\Petchdy\IdeaProjects\test
+        System.out.printf("Hello world\nMy name is Petch\nI'm 15 years old");
+    }
+}
